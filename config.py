a62abed98cf9987ff5eee1e222d2992c3221486a
TimurBoss <prostoykent1@gmail.com>
2025-06-28 15:41:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 token = ['8120336909:AAG-wGSo7I-KQyQdK1vynF6FVHEk8IinFmg'] #  @qwegamebot , @test
 owner_id = 1771886212
-bot_name = "qwegamebot"
+bot_name = "qwegamesbot"
 owner = '<a href="t.me/Timur06034/">Тимурка</a>'
 chat = '<a href="t.me/qwechat/">QWE | Игровой чат #1</a>'
 channel = '<a href="t.me/qwechannel/">QWE CHANNEL | Канал</a>'
